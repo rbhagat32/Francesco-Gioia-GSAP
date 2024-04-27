505c6c7eea13da92cbd85f0193aecd4f88530f5a
rbhagat32 <raghavbhagat32@gmail.com>
2024-04-27 20:37:15 +0530

    loader added

--- script.js
+++ script.js
@@ -4,30 +4,30 @@ tl.from('nav h1, .links button, .links a', {
     y: -20,
     duration: .4,
     opacity: 0,
-    stagger: .4,
-    delay: .8,
+    stagger: .2,
+    delay: 4,
 })
 
 tl.from('.text h1', {
     x: -200,
     opacity: 0,
     duration: .4,
-    stagger: .4,
+    stagger: .2,
 })
 
 tl.from('.images img', {
-    x: 300,
+    x: 150,
     rotate: 45,
     opacity: 0,
-    duration: .6,
-    stagger: .6,
+    duration: .4,
+    stagger: .3,
 })
 
 tl.from('footer h2', {
     y: 80,
     opacity: 0,
     duration: .4,
-    stagger: .4,
+    stagger: .2,
 })
 
 
@@ -42,3 +42,8 @@ const btn = document.querySelector('button');
 btn.addEventListener('click', () => {
     document.body.classList.toggle('light-mode');
 });
+
+// Loader Animation
+setTimeout(function(){
+    document.querySelector("#loader").style.top = "-100%";
+}, 3500);
