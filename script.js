033029b2268cb5ca6c75b55ceafa9bbedf724610
rbhagat32 <raghavbhagat32@gmail.com>
2024-04-28 00:57:03 +0530

    backup

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ let tl = gsap.timeline();
 
 tl.from('nav h1, .links button, .links a', {
     y: -20,
-    duration: .4,
     opacity: 0,
-    stagger: .2,
     delay: 4,
+    duration: .4,
+    stagger: .2,
 })
 
 tl.from('.text h1', {
@@ -30,6 +30,46 @@ tl.from('footer h2', {
     stagger: .2,
 })
 
+tl.to('.text h1', {
+    x: 200,
+    opacity: 0,
+    delay: 1,
+    duration: .4,
+    stagger: .2,
+})
+
+tl.to('.text', {
+    y: 1000,
+})
+
+tl.to('#img1', {
+    x: -520,
+    y: 100,
+    scale: 1.5,
+    rotate: 0,
+    duration: .6,
+})
+
+tl.to('#img2', {
+    x: 480,
+    y: 100,
+    scale: 1.5,
+    rotate: 0,
+    duration: .6,
+})
+
+tl.to('#img3', {
+    x: -20,
+    y: 100,
+    scale: 1.5,
+    rotate: 0,
+    duration: .6,
+})
+
+document.querySelector('#img1').addEventListener('mousemove', function(dets) {
+    console.log(dets.x, dets.y);
+
+})
 
 // Text Selection Disable
 document.addEventListener("selectstart", (e) => {
